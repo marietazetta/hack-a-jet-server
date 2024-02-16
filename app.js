b4853941637c4fe8a0b74878d464d9b22fbbde26
luisjunco <38411763+luisjunco@users.noreply.github.com>
2024-02-16 15:39:50 +0100

    fix port and gitignore

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(morgan("dev"));
